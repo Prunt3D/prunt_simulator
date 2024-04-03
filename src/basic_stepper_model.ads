bfd96f208ab56d73a09aa1c6274b17812e603480
Liam Powell <liam@liampwll.com>
2024-04-03 22:07:19 +0800

    Volatile all the things.

--- src/basic_stepper_model.ads
+++ src/basic_stepper_model.ads
@@ -4,10 +4,10 @@ with Prunt_Glue;     use Prunt_Glue;
 package Basic_Stepper_Model is
 
    type Stepper_Type is record
-      Pos         : Length;
-      Mm_Per_Step : Length;
-      Dir         : Pin_State := Low_State;
-      Step        : Pin_State := Low_State;
+      Pos         : Length with Volatile;
+      Mm_Per_Step : Length with Volatile;
+      Dir         : Pin_State := Low_State with Volatile;
+      Step        : Pin_State := Low_State with Volatile;
    end record;
 
    procedure Take_Step (Stepper : in out Stepper_Type);
--- src/basic_thermal_model.ads
+++ src/basic_thermal_model.ads
@@ -3,12 +3,12 @@ with Physical_Types; use Physical_Types;
 package Basic_Thermal_Model is
 
    type Block_Type is record
-      Heater_PWM            : PWM_Scale;
-      Heater_Max_Power      : Power;
-      Block_Heat_Capacity   : Heat_Capacity;
-      Block_Temperature     : Temperature;
-      Block_To_Air_Transfer : Specific_Heat_Transfer_Coefficient;
-      Air_Temperature       : Temperature;
+      Heater_PWM            : PWM_Scale with Volatile;
+      Heater_Max_Power      : Power with Volatile;
+      Block_Heat_Capacity   : Heat_Capacity with Volatile;
+      Block_Temperature     : Temperature with Volatile;
+      Block_To_Air_Transfer : Specific_Heat_Transfer_Coefficient with Volatile;
+      Air_Temperature       : Temperature with Volatile;
    end record;
 
    procedure Update (Block : in out Block_Type; Step : Time);
